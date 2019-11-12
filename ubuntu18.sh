58ee15bca4a18b94efdb9bc7e5ed1275ef372df4
Xt <pbt39ppc@gmail.com>
2019-11-13 01:37:04 +0200

    Update ubuntu18.sh

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -1,3 +1,3 @@
 sudo su -
 apt update && apt upgrade
-apt install git htop mc build-essential smartmontools
+apt install git htop mc build-essential smartmontools nginx php-fpm letsencrypt python3-certbot-nginx
